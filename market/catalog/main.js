cee105b5f60b48907dba8602b26126a823aa9a9d
joojdev <jvpiovezan2905@gmail.com>
2022-09-07 20:33:37 -0300

    Added more products

--- market/catalog/main.js
+++ market/catalog/main.js
@@ -13,6 +13,62 @@ const items = [
     image: 'dorflex.jpg',
     name: 'Dorflex 36 comprimidos',
     price: 12.99
+  },
+  {
+    image: 'website.png',
+    name: 'site super original e criativo',
+    price: 0,
+    stretch: true
+  },
+  {
+    image: 'alprazolam.jpg',
+    name: 'Alprazolam 1mg',
+    price: 41.9
+  },
+  {
+    image: 'melatonina.jpg',
+    name: 'Melatonina 60 comprimidos',
+    price: 34.99
+  },
+  {
+    image: 'bala.jpg',
+    name: 'Bala de matar o amiguinho',
+    price: 7.99
+  },
+  {
+    image: 'trembolona.png',
+    name: 'Bomba pra ficar GRANDÃO',
+    price: 89.99
+  },
+  {
+    image: 'ricardo.png',
+    name: 'prof ricardo',
+    price: -1,
+    stretch: true
+  },
+  {
+    image: 'leite-ninho.jpg',
+    name: 'gato cheirador de leite ninho',
+    price: 0,
+    stretch: true
+  },
+  {
+    image: 'gato-miojo.jpg',
+    name: 'gato miojo :O (promoção)',
+    price: 0,
+    stretch: true
+  },
+  {
+    image: 'gato-triste.jpg',
+    name: 'gato triste :((',
+    price: 0,
+    stretch: true
+  },
+  {
+    image: 'gato-incel.jpg',
+    name: 'gato incel >:(',
+    price: 0,
+    stretch: true
   }
 ]
 
@@ -36,10 +92,26 @@ function createElement(tag, textContent, className, backgroundImage) {
   return element
 }
 
-items.forEach(({ image, name, price }) => {
-  const imageElement = createElement('div', null, 'image', `../../images/${image}`)
+items.forEach(({ image, name, price, stretch }) => {
+  let imageClass = 'image'
+
+  if (stretch) {
+    imageClass = 'image stretch'
+  }
+
+  const imageElement = createElement('div', null, imageClass, `../../images/${image}`)
   const nameElement = createElement('p', name, 'name')
-  const priceElement = createElement('p', `R$${price}`.replace('.', ','), 'price')
+
+  let priceTag = 'pode levar'
+
+  if (price > 0) {
+    priceTag = `R$${price.toFixed(2)}`.replace('.', ',')
+  } else if (price < 0) {
+    priceTag = 'não está à venda só achei bonita essa foto'
+  }
+
+  const priceElement = createElement('p', priceTag, 'price')
+
   const buttonElement = createElement('button', 'Comprar')
 
   const itemElement = createElement('li', null, 'item')
@@ -50,3 +122,8 @@ items.forEach(({ image, name, price }) => {
 
   itemsElement.append(itemElement)
 })
+
+if (items.length % 2 == 1) {
+  const hiddenElement = createElement('div', null, 'hidden')
+  itemsElement.append(hiddenElement)
+}
